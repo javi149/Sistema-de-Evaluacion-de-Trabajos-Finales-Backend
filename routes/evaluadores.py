c38f15e1d36ca183394e54c8b57d18d0830a71ad
adanramos-cmd <adanramos@alu.unach.cl>
2025-11-22 15:33:13 -0300

    Update evaluadores.py
    Arreglo a la ruta evaluadores

--- routes/evaluadores.py
+++ routes/evaluadores.py
@@ -1,4 +1,30 @@
+from flask import Blueprint, jsonify, request
+from app import db
+from models import Evaluador
 
-from flask import Blueprint
-evaluadores_bp = Blueprint('evaluadores_bp', __name__, url_prefix='/api/evaluadores')
+evaluadores_bp = Blueprint('evaluadores', __name__, url_prefix='/evaluadores')
 
+@evaluadores_bp.route('/', methods=['GET'])
+def listar_evaluadores():
+    evaluadores = Evaluador.query.all()
+    resultado = [{
+        "id": e.evaluadores_id, 
+        "nombre": e.nombre, 
+        "email": e.email,
+        "rol": e.rol,  # Tutor, Jurado
+        "tipo": e.tipo # Interno, Externo
+    } for e in evaluadores]
+    return jsonify(resultado)
+
+@evaluadores_bp.route('/', methods=['POST'])
+def crear_evaluador():
+    data = request.get_json()
+    nuevo = Evaluador(
+        nombre=data.get('nombre'),
+        email=data.get('email'),
+        rol=data.get('rol'),
+        tipo=data.get('tipo')
+    )
+    db.session.add(nuevo)
+    db.session.commit()
+    return jsonify({"mensaje": "Evaluador creado", "id": nuevo.evaluadores_id}), 201
